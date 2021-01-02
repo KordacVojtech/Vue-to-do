9c4d89c542d146fddc84573486b70c5d99c0247f
KordacVojtech <v_kordac@utb.cz>
2021-01-02 16:43:13 +0100

    'Deploy'

---
+++ todovue/src/components/HeaderTop.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <h1 class="title">Vue To-Do List</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .header{
+        background: linear-gradient(rgb(54, 192, 150), rgb(55, 202, 75));
+        height: 5rem;
+        width: 110%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin-top: -4rem;
+        color: white;
+        margin-left: -10px;
+        margin-right: -30px;
+    }
+    .title{
+        margin-left: 10px;
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
